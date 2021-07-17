8d6d39e15cb6eb4aa27ac19214511b5dc32cda15
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-07-17 19:37:38 +0100

    17/7

--- javaScriptArray/test.js
+++ javaScriptArray/test.js
@@ -7,3 +7,14 @@ let num= add(2,2);
 
 console.log(add(1,2));
 console.log(num)
+
+const array = [];
+setTimeout(() => {
+
+  setTimeout(() => {
+    array.push('it worked');
+  }, 0);
+
+}, 0);
+
+console.log( array );
