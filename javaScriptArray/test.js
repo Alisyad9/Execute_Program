d05836638923c1997d6678d199306ffd8926d3cd
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-10-09 23:44:15 +0100

    execute program: code testing

--- javaScriptArray/test.js
+++ javaScriptArray/test.js
@@ -1,20 +1,37 @@
+// function add(a,b){
+//     return a + b;
+// }
 
-function add(a,b){
-    return a + b;
+// let num= add(2,2);
+
+// console.log(add(1,2));
+// console.log(num)
+
+// const array = [];
+// setTimeout(() => {
+
+//   setTimeout(() => {
+//     array.push('it worked');
+//   }, 0);
+
+// }, 0);
+
+// console.log( array );
+
+const users = [
+  { id: 1, name: "Amir" },
+  { id: 2, name: "Betty" },
+];
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    const user = users.find((user) => user.id === id);
+    if (user) {
+      resolve(user);
+    } else {
+      reject(new Error("no such user"));
+    }
+  });
 }
 
-let num= add(2,2);
-
-console.log(add(1,2));
-console.log(num)
-
-const array = [];
-setTimeout(() => {
-
-  setTimeout(() => {
-    array.push('it worked');
-  }, 0);
-
-}, 0);
-
-console.log( array );
+console.log(getUser(1));
